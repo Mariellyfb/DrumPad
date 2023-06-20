d6597b615e2f93d6a76c14685a901835643c2764
Mariellyfb <132379411+Mariellyfb@users.noreply.github.com>
2023-06-20 18:30:33 +0200

    Update script.js

--- proyectoBateria/js/script.js
+++ proyectoBateria/js/script.js
@@ -1,1 +1,21 @@
 "use strict"
+
+let crashSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\crash.wav");
+
+let hihatcSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\hihat-close.wav");
+
+let hihatoSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\hihat-open.wav");
+
+let kickSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\kick.wav");
+
+let rideSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\ride.wav");
+
+let snareSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\snare.wav");
+
+let tomhSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\tom-high.wav");
+
+let tomlSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\tom-low.wav");
+
+let tommSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\tom-mid.wav");
+
+const buttons = 
