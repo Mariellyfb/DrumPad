cbb2461b5920b679c74c305be34839ee80bbe20a
RocioTamy <rosio90mlg@gmail.com>
2023-06-20 20:03:05 +0200

    Update script.js

--- proyectoBateria/js/script.js
+++ proyectoBateria/js/script.js
@@ -1,28 +1,56 @@
-"use strict"
 
-const crashSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\crash.wav");
-
-const hihatcSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\hihat-close.wav");
-
-const hihatoSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\hihat-open.wav");
-
-const kickSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\kick.wav");
-
-const rideSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\ride.wav");
-
-const snareSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\snare.wav");
-
-const tomhSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\tom-high.wav");
-
-const tomlSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\tom-low.wav");
-
-const tommSound = new Audio ("C:\Users\User\OneDrive\Documents\GitHub\Bateria-JS-grupo-3\proyectoBateria\sounds\tom-mid.wav");
-
-const buttons = document.getElementsByClassName("botones");
 
 // codigo que coje el primer boton en una variable y luego el sonido en otra y hace una funcion click
-  const button = document.getElementById("boton1");
+  const button1 = document.getElementById("boton1");
 const sound1 = new Audio("./sounds/crash.wav");
-button.addEventListener("click", () => {
+button1.addEventListener("click", () => {
     sound1.play();
+});
+
+const button2 = document.getElementById("boton2");
+const sound2 = new Audio("./sounds/hihat-close.wav");
+button2.addEventListener("click", () => {
+    sound2.play();
+});
+
+const button3 = document.getElementById("boton3");
+const sound3 = new Audio("./sounds/hihat-open.wav");
+button3.addEventListener("click", () => {
+    sound3.play();
+});
+
+const button4 = document.getElementById("boton4");
+const sound4 = new Audio("./sounds/kick.wav");
+button4.addEventListener("click", () => {
+    sound4.play();
+});
+
+const button5 = document.getElementById("boton5");
+const sound5 = new Audio("./sounds/ride.wav");
+button5.addEventListener("click", () => {
+    sound5.play();
+});
+
+const button6 = document.getElementById("boton6");
+const sound6 = new Audio("./sounds/snare.wav");
+button6.addEventListener("click", () => {
+    sound6.play();
+});
+
+const button7 = document.getElementById("boton7");
+const sound7 = new Audio("./sounds/tom-high.wav");
+button7.addEventListener("click", () => {
+    sound7.play();
+});
+
+const button8 = document.getElementById("boton8");
+const sound8 = new Audio("./sounds/tom-low.wav");
+button8.addEventListener("click", () => {
+    sound8.play();
+});
+
+const button9 = document.getElementById("boton9");
+const sound9 = new Audio("./sounds/tom-mid.wav");
+button9.addEventListener("click", () => {
+    sound9.play();
 });
