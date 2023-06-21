7eea9ed8d9469edb80c86a05a707f28fa584e89a
Mariellyfb <132379411+Mariellyfb@users.noreply.github.com>
2023-06-21 12:54:29 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,55 +1,56 @@
 
 
 // codigo que coje el primer boton en una variable y luego el sonido en otra y hace una funcion click
-  const button1 = document.getElementById("boton1");
+
+const button1 = document.getElementById("btn1");
 const sound1 = new Audio("./sounds/crash.wav");
 button1.addEventListener("click", () => {
     sound1.play();
 });
 
-const button2 = document.getElementById("boton2");
+const button2 = document.getElementById("btn2");
 const sound2 = new Audio("./sounds/hihat-close.wav");
 button2.addEventListener("click", () => {
     sound2.play();
 });
 
-const button3 = document.getElementById("boton3");
+const button3 = document.getElementById("btn3");
 const sound3 = new Audio("./sounds/hihat-open.wav");
 button3.addEventListener("click", () => {
     sound3.play();
 });
 
-const button4 = document.getElementById("boton4");
+const button4 = document.getElementById("btn4");
 const sound4 = new Audio("./sounds/kick.wav");
 button4.addEventListener("click", () => {
     sound4.play();
 });
 
-const button5 = document.getElementById("boton5");
+const button5 = document.getElementById("btn5");
 const sound5 = new Audio("./sounds/ride.wav");
 button5.addEventListener("click", () => {
     sound5.play();
 });
 
-const button6 = document.getElementById("boton6");
+const button6 = document.getElementById("btn6");
 const sound6 = new Audio("./sounds/snare.wav");
 button6.addEventListener("click", () => {
     sound6.play();
 });
 
-const button7 = document.getElementById("boton7");
+const button7 = document.getElementById("btn7");
 const sound7 = new Audio("./sounds/tom-high.wav");
 button7.addEventListener("click", () => {
     sound7.play();
 });
 
-const button8 = document.getElementById("boton8");
+const button8 = document.getElementById("btn8");
 const sound8 = new Audio("./sounds/tom-low.wav");
 button8.addEventListener("click", () => {
     sound8.play();
 });
 
-const button9 = document.getElementById("boton9");
+const button9 = document.getElementById("btn9");
 const sound9 = new Audio("./sounds/tom-mid.wav");
 button9.addEventListener("click", () => {
     sound9.play();
