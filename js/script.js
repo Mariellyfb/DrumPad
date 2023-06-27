91a01bcdb929f116cb20a22b8f013e87dd98ba2e
bobgus39 <bobgus39@gmail.com>
2023-06-27 14:02:50 +0200

    rec

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 'use strict';
+
 // todos los getElementById()
 const button1 = document.getElementById('btn1');
 const button2 = document.getElementById('btn2');
@@ -9,136 +10,115 @@ const button6 = document.getElementById('btn6');
 const button7 = document.getElementById('btn7');
 const button8 = document.getElementById('btn8');
 const button9 = document.getElementById('btn9');
+
 const backElement = document.getElementById('atras');
+const recElement = document.getElementById('grabar');
+const playElement = document.getElementById('play');
+
+let recording = false;
+
+let recStartTime;
+let songNotes = [];
+
+// Obtener el botón y el contenido del botón desplegable
+const Btn = document.querySelector('.inst-btn');
+const Content = document.querySelector('.ul-content');
+
+const soundElement = (letraPulsada, audio) => {
+  recFun(letraPulsada);
+  let sound = new Audio(audio);
+  //button1.classList.add = 'active';
+  sound.play();
+};
 
 // Todos los audios
-const sound1 = new Audio('./sounds/crash.wav');
-const sound2 = new Audio('./sounds/hihat-close.wav');
-const sound3 = new Audio('./sounds/hihat-open.wav');
-const sound4 = new Audio('./sounds/kick.wav');
-const sound5 = new Audio('./sounds/ride.wav');
-const sound6 = new Audio('./sounds/snare.wav');
-const sound7 = new Audio('./sounds/tom-high.wav');
-const sound8 = new Audio('./sounds/tom-low.wav');
-const sound9 = new Audio('./sounds/tom-mid.wav');
+const soundElementWrapper = (letra) => {
+  switch (letra.toUpperCase()) {
+    case 'C':
+      soundElement(letra, './sounds/tom-mid.wav');
+      break;
+    case 'X':
+      soundElement(letra, './sounds/tom-low.wav');
+      break;
+    case 'Z':
+      soundElement(letra, './sounds/tom-high.wav');
+      break;
+    case 'D':
+      soundElement(letra, './sounds/snare.wav');
+      break;
+    case 'S':
+      soundElement(letra, './sounds/ride.wav');
+      break;
+    case 'A':
+      soundElement(letra, './sounds/kick.wav');
+      break;
+    case 'E':
+      soundElement(letra, './sounds/hihat-open.wav');
+      break;
+    case 'W':
+      soundElement(letra, './sounds/hihat-close.wav');
+      break;
+    case 'Q':
+      soundElement(letra, './sounds/crash.wav');
+      break;
+  }
+};
+
+const recFun = (char) => {
+  if (isRecording()) {
+    recordNote({
+      key: char,
+      startTime: Date.now() - recStartTime,
+    });
+  }
+};
 
 // codigo que coje el primer boton en una variable y luego el sonido en otra y hace una funcion click para que suenen cada boton
-
 button1.addEventListener('click', () => {
-  sound1.play();
+  soundElementWrapper('Q');
 });
 
 button2.addEventListener('click', () => {
-  sound2.play();
+  soundElementWrapper('W');
 });
 
 button3.addEventListener('click', () => {
-  sound3.play();
+  soundElementWrapper('E');
 });
 
 button4.addEventListener('click', () => {
-  sound4.play();
+  soundElementWrapper('A');
 });
 
 button5.addEventListener('click', () => {
-  sound5.play();
+  soundElementWrapper('S');
 });
 
 button6.addEventListener('click', () => {
-  sound6.play();
+  soundElementWrapper('D');
 });
 
 button7.addEventListener('click', () => {
-  sound7.play();
+  soundElementWrapper('Z');
 });
 
 button8.addEventListener('click', () => {
-  sound8.play();
+  soundElementWrapper('X');
 });
 
 button9.addEventListener('click', () => {
-  sound9.play();
+  soundElementWrapper('C');
 });
 
 //controles teclado
 
-document.addEventListener('keydown', (event) => {
+document.addEventListener('keydown', (e) => playNote(e));
+
+function playNote(event) {
   const letraPulsada = String.fromCharCode(event.keyCode);
   console.log('La tecla seleccionada es:', letraPulsada);
-  switch (letraPulsada) {
-    case 'C':
-    case 'c':
-      let sound9 = new Audio('./sounds/tom-mid.wav');
-      button9.style.filter = 'brightness(125%)';
-      sound9.play();
-
-      break;
-
-    case 'X':
-    case 'x':
-      let sound8 = new Audio('./sounds/tom-low.wav');
-      button8.style.filter = 'brightness(125%)';
-      sound8.play();
-
-      break;
-
-    case 'Z':
-    case 'z':
-      let sound7 = new Audio('./sounds/tom-high.wav');
-      button7.style.filter = 'brightness(125%)';
-      sound7.play();
-
-      break;
-
-    case 'D':
-    case 'd':
-      let sound6 = new Audio('./sounds/snare.wav');
-      button6.style.filter = 'brightness(125%)';
-      sound6.play();
-
-      break;
-
-    case 'S':
-    case 's':
-      let sound5 = new Audio('./sounds/ride.wav');
-      button5.style.filter = 'brightness(125%)';
-      sound5.play();
-
-      break;
-
-    case 'A':
-    case 'a':
-      let sound4 = new Audio('./sounds/kick.wav');
-      button4.style.filter = 'brightness(125%)';
-      sound4.play();
-
-      break;
-
-    case 'E':
-    case 'e':
-      let sound3 = new Audio('./sounds/hihat-open.wav');
-      button3.style.filter = 'brightness(125%)';
-      sound3.play();
-
-      break;
-
-    case 'W':
-    case 'w':
-      let sound2 = new Audio('./sounds/hihat-close.wav');
-      button2.style.filter = 'brightness(125%)';
-      sound2.play();
-
-      break;
-
-    case 'Q':
-    case 'q':
-      let sound1 = new Audio('./sounds/crash.wav');
-      button1.style.filter = 'brightness(125%)';
-      sound1.play();
-
-      break;
-  }
-});
+  soundElementWrapper(letraPulsada);
+}
 
 document.addEventListener('keyup', (event) => {
   const letraPulsada = String.fromCharCode(event.keyCode);
@@ -146,67 +126,62 @@ document.addEventListener('keyup', (event) => {
   switch (letraPulsada) {
     case 'C':
     case 'c':
-      button9.style.filter = 'brightness(100%)';
-
+      button9.classList.remove = 'active';
       break;
 
     case 'X':
     case 'x':
-      button8.style.filter = 'brightness(100%)';
+      button8.classList.remove = 'active';
 
       break;
 
     case 'Z':
     case 'z':
-      button7.style.filter = 'brightness(100%)';
+      button7.classList.remove = 'active';
 
       break;
 
     case 'D':
     case 'd':
-      button6.style.filter = 'brightness(100%)';
+      button6.classList.remove = 'active';
 
       break;
 
     case 'S':
     case 's':
-      button5.style.filter = 'brightness(100%)';
+      button5.classList.remove = 'active';
 
       break;
 
     case 'A':
     case 'a':
-      button4.style.filter = 'brightness(100%)';
+      button4.classList.remove = 'active';
 
       break;
 
     case 'E':
     case 'e':
-      button3.style.filter = 'brightness(100%)';
+      button3.classList.remove = 'active';
 
       break;
 
     case 'W':
     case 'w':
-      button2.style.filter = 'brightness(100%)';
+      button2.classList.remove = 'active';
 
       break;
 
     case 'Q':
     case 'q':
-      button1.style.filter = 'brightness(100%)';
+      button1.classList.remove = 'active';
 
       break;
   }
 });
 
-// Obtener el botón y el contenido del botón desplegable
-const Btn = document.querySelector('.inst-btn');
-const Content = document.querySelector('ul.ul-content');
-
 // Mostrar/ocultar el contenido del botón desplegable al hacer clic en el botón
 Btn.addEventListener('click', function () {
-  Content.style.display = Content.style.display === 'flex' ? 'none' : 'flex';
+  Content.style.display = Content.style.display === 'block' ? 'none' : 'block';
 });
 
 //Funciones
@@ -217,58 +192,65 @@ backElement.addEventListener('click', (event) => {
   redirectToOtherPage('index.html');
 });
 
-// boton play url
-/*const playElement = document.getElementById("play");
-playElement.addEventListener('click', (event) => {
-    redirectToOtherPage("batery.html");
-});*/
+recElement.addEventListener('click', toggleRec);
+playElement.addEventListener('click', toggleRec);
 
-// funcion para redireccionar URL
 function redirectToOtherPage(page) {
   window.location.href = page;
 }
 
-/*Probando a hacer el Rec y el stop y no funciona, pero se necesita hacer un tiempo y meterlo junto a la tecla presionada en ese tiempo en un objeto, despues este objeto es el mapa para replicar los sonidos 
-
-let recBtn = document.getElementById("grabar");
-recBtn.addEventListener('click', (event) => {
-
-getTimeElapsed();
-
-let recObject = {
-    time: [timeDiff] ,
-    key: [letraPulsada]
+function toggleRec() {
+  recElement.classList.toggle('active');
+  if (isRecording()) {
+    startRec();
+  } else {
+    stopRec();
+  }
 }
 
-let recTime = recObject.time;
-let recKey = recObject.key;
+function isRecording() {
+  return recElement != null && recElement.classList.contains('active');
+}
 
-let stopBtn = document.getElementById
-("parar");
-stopBtn.addEventListener('click', (event) => {
-    if (event) {
-    event.preventDefault();
-    console.log("Acabas de realizar un click en el stop");
-    let ic = 0;
-    } else ic = 1
-});
+function startRec() {
+  recStartTime = Date.now();
+  songNotes = [];
+  playElement.classList.remove('show');
+}
 
+function stopRec() {
+  playSong();
+  playElement.classList.add('show');
+}
 
-for (let i = 1; i != 1; i = ic){
-    recTime.push(timeDiff);
-    recKey.push(letraPulsada);
-    console.log(recObject);
-} 
+function playSong() {
+  console.log('songNotes inicial:', songNotes);
+  readPlay();
+}
 
-});
+function recordNote(note) {
+  songNotes.push(note);
+}
 
-function getTimeElapsed() {
-    const startTime = new Date(); // Obtiene la hora de inicio
+function readPlay() {
+  let timePlay = 0;
 
-    setInterval(() => {
-      const currentTime = new Date(); // Obtiene la hora actual
-      const timeDiff = currentTime - startTime; // Calcula la diferencia de tiempo en milisegundos
+  const playInterval = setInterval(() => {
+    let i = 0;
+    while (i < songNotes.length && songNotes[i].startTime < timePlay) {
+      console.log(
+        `Sonido ${songNotes[i].key} ${songNotes[i].startTime} (${timePlay})`
+      );
+      soundElementWrapper(songNotes[i].key);
+      songNotes.splice(i, 1);
+      i++;
+    }
 
-    console.log("la hora es:", timeDiff);
-    }, 5000); // Imprime cada 5 segundos
-};*/
+    if (songNotes.length === 0) {
+      console.log('STOP');
+      clearInterval(playInterval);
+    }
+
+    timePlay += 500;
+  }, 500);
+}
